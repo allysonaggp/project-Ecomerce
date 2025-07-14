d3a2b72fec6133eb312358f6813f1197df55e5aa
allysonaggp <allysonaggp@gmail.com>
2025-07-14 16:39:16 -0300

    adicionado funcionalidade do botao remover do carrinho de compras

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+//abrir barra de navegação no modo mobile
 const navbar = document.querySelector("nav");
 const menuButton = document.querySelector(".menu-button");
 
 menuButton.addEventListener("click", () => {
   navbar.classList.toggle("show-menu");
 });
+
+
+//carrinho de compras
+
+const removeProductButtons = document.getElementsByClassName("remove-product-button");
+for (let i = 0; i < removeProductButtons.length; i++) {
+  removeProductButtons[i].addEventListener("click", function (event) {
+    //console.log(event.target)
+    event.target.parentElement.parentElement.remove()
+  })
+}
