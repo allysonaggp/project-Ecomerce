e736e50f39fc24599a47251d2f71948dfa7ce5c8
allysonaggp <allysonaggp@gmail.com>
2025-07-14 17:26:29 -0300

    adicionado JS do botao remover todos os itens

--- js/script.js
+++ js/script.js
@@ -9,10 +9,22 @@ menuButton.addEventListener("click", () => {
 
 //carrinho de compras
 
+//botao de remover itens do carrinho de compras
 const removeProductButtons = document.getElementsByClassName("remove-product-button");
 for (let i = 0; i < removeProductButtons.length; i++) {
   removeProductButtons[i].addEventListener("click", function (event) {
     //console.log(event.target)
     event.target.parentElement.parentElement.remove()
+  })
+}
+
+//botao de remover todos os itens do carrinho de compras
+
+const removeAllProductButton = document.getElementsByClassName("remove-all-product-button");
+//console.log(removeAllProductButton)
+for (let i = 0; i < removeAllProductButton.length; i++) {
+  removeAllProductButton[i].addEventListener("click", function (event) {
+    //console.log("clicou")
+    event.target.closest('.box-info-sell').querySelectorAll('.product-quantity').forEach(element => element.remove()) ;
   })
 }
